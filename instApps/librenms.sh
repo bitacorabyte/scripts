bb5c6b8eac7989984da07683a02c70b788d51553
bitacorabyte <bitacorabyte@gmail.com>
2024-12-10 13:15:48 +0100

    Correción formato color echo

--- instApps/librenms.sh
+++ instApps/librenms.sh
@@ -22,46 +22,46 @@
 # 15. Habilittar el horario
 # 16. Configurar logrotate
 ##
-echo "\033[0;32mEste script instala LibreNMS. Developed on LXC Debian 12\032[0m"
-echo "\033[0;32m###########################################################\032[0m"
-echo "\033[0;32mPASO 1. CONFIGURANDO EL TIMEZONE\032[0m"
-echo "\033[0;32m###########################################################\032[0m"
+echo "\033[0;32mEste script instala LibreNMS. Developed on LXC Debian 12"
+echo "\033[0;32m###########################################################"
+echo "\033[0;32mPASO 1. CONFIGURANDO EL TIMEZONE"
+echo "\033[0;32m###########################################################"
 # Set the system timezone
 sleep 3
 timedatectl set-timezone "Europe/Madrid"
 echo " "
-echo "\033[0;32m###########################################################\032[0m"
-echo "\033[0;32mPASO 2. ACTUALIZAR SO\032[0m"
-echo "\033[0;32m###########################################################\032[0m"
+echo "\033[0;32m###########################################################"
+echo "\033[0;32mPASO 2. ACTUALIZAR SO"
+echo "\033[0;32m###########################################################"
 # Actualizar SO
 sleep 3
 apt update && apt upgrade -y
 echo " "
-echo "\033[0;32m###########################################################\032[0m"
-echo "\033[0;32mPASO 3. INSTALAR PAQUETES REQUERIDOS\032[0m"
-echo "\033[0;32m###########################################################\032[0m"
+echo "\033[0;32m###########################################################"
+echo "\033[0;32mPASO 3. INSTALAR PAQUETES REQUERIDOS"
+echo "\033[0;32m###########################################################"
 # Instalar paquetes
 sleep 3
 apt install -y apt-transport-https lsb-release ca-certificates wget acl curl fping git graphviz imagemagick mariadb-client mariadb-server mtr-tiny nginx-full nmap php-cli php-curl php-fpm php-gd php-gmp php-mb>
 echo " "
-echo "\033[0;32m###########################################################\032[0m"
-echo "\033[0;32mPASO 4. CREAR USUARIO LIBRENMS\032[0m"
-echo "\033[0;32m###########################################################\032[0m"
+echo "\033[0;32m###########################################################"
+echo "\033[0;32mPASO 4. CREAR USUARIO LIBRENMS"
+echo "\033[0;32m###########################################################"
 # Crear usuario librenms
 sleep 3
 useradd librenms -d /opt/librenms -M -r -s "$(which bash)"
 echo " "
-echo "\033[0;32m###########################################################\032[0m"
-echo "\033[0;32mPASO 5. DESCARGAR LIBRENMS\032[0m"
-echo "\033[0;32m###########################################################\032[0m"
+echo "\033[0;32m###########################################################"
+echo "\033[0;32mPASO 5. DESCARGAR LIBRENMS"
+echo "\033[0;32m###########################################################"
 # Download LibreNMS
 sleep 3
 cd /opt
 git clone https://github.com/librenms/librenms.git
 echo " "
-echo "\033[0;32m###########################################################\032[0m"
-echo "\033[0;32mPASO 6. ESTABLECER PERMISOS\032[0m"
-echo "\033[0;32m###########################################################\032[0m"
+echo "\033[0;32m###########################################################"
+echo "\033[0;32mPASO 6. ESTABLECER PERMISOS"
+echo "\033[0;32m###########################################################"
 # Set permissions
 sleep 3
 chown -R librenms:librenms /opt/librenms
@@ -69,16 +69,16 @@ chmod 771 /opt/librenms
 setfacl -d -m g::rwx /opt/librenms/rrd /opt/librenms/logs /opt/librenms/bootstrap/cache/ /opt/librenms/storage/
 setfacl -R -m g::rwx /opt/librenms/rrd /opt/librenms/logs /opt/librenms/bootstrap/cache/ /opt/librenms/storage/
 echo " "
-echo "\033[0;32m###########################################################\032[0m"
-echo "\033[0;32mPASO 7. INSTALAR DEPENDENCIAS DE PHP\032[0m"
-echo "\033[0;32m###########################################################\032[0m"
+echo "\033[0;32m###########################################################"
+echo "\033[0;32mPASO 7. INSTALAR DEPENDENCIAS DE PHP"
+echo "\033[0;32m###########################################################"
 # Install PHP dependencies
 sleep 3
 su librenms bash -c '/opt/librenms/scripts/composer_wrapper.php install --no-dev'
 echo " "
-echo "\033[0;32m###########################################################\032[0m"
-echo "\033[0;32mPASO 8. CONFIGURAR MARIADB\032[0m"
-echo "\033[0;32m###########################################################\032[0m"
+echo "\033[0;32m###########################################################"
+echo "\033[0;32mPASO 8. CONFIGURAR MARIADB"
+echo "\033[0;32m###########################################################"
 # Configure MariaDB
 sleep 3
 mysql -uroot -e "CREATE DATABASE librenms CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
@@ -90,9 +90,9 @@ sed -i '/mysqld]/ a innodb_file_per_table=1' /etc/mysql/mariadb.conf.d/50-server
 systemctl restart mariadb
 systemctl enable mariadb
 echo " "
-echo "\033[0;32m###########################################################\032[0m"
-echo "\033[0;32mPASO 9. CONFIGURAR PHP-FPM\032[0m"
-echo "\033[0;32m###########################################################\032[0m"
+echo "\033[0;32m###########################################################"
+echo "\033[0;32mPASO 9. CONFIGURAR PHP-FPM"
+echo "\033[0;32m###########################################################"
 # Configure PHP-FPM
 sleep 3
 cp /etc/php/8.2/fpm/pool.d/www.conf /etc/php/8.2/fpm/pool.d/librenms.conf
@@ -101,9 +101,9 @@ sed -i 's/user = www-data/user = librenms/' /etc/php/8.2/fpm/pool.d/librenms.con
 sed -i 's/group = www-data/group = librenms/' /etc/php/8.2/fpm/pool.d/librenms.conf
 sed -i 's/listen = \/run\/php\/php8.2-fpm.sock/listen = \/run\/php-fpm-librenms.sock/' /etc/php/8.2/fpm/pool.d/librenms.conf
 echo " "
-echo "\033[0;32m###########################################################\032[0m"
-echo "\033[0;32mPASO 10. ESTABLECER TIMEZONE EN PHP\032[0m"
-echo "\033[0;32m###########################################################\032[0m"
+echo "\033[0;32m###########################################################"
+echo "\033[0;32mPASO 10. ESTABLECER TIMEZONE EN PHP"
+echo "\033[0;32m###########################################################"
 # Set timezone in php.ini
 sleep 3
 sed -i "/;date.timezone =/ a date.timezone = Europe/Madrid" /etc/php/8.2/fpm/php.ini
@@ -113,9 +113,9 @@ echo " "
 ### restart PHP-fpm ###
 systemctl restart php8.2-fpm
 echo " "
-echo "\033[0;32m###########################################################\032[0m"
-echo "\033[0;32mPASO 11. CONFIGURAR NGINX\032[0m"
-echo "\033[0;32m###########################################################\032[0m"
+echo "\033[0;32m###########################################################"
+echo "\033[0;32mPASO 11. CONFIGURAR NGINX"
+echo "\033[0;32m###########################################################"
 # Configure Web Server
 sleep 3
 echo "server {"> /etc/nginx/conf.d/librenms.conf
@@ -144,17 +144,17 @@ rm /etc/nginx/sites-enabled/default
 systemctl restart nginx
 systemctl restart php8.2-fpm
 echo " "
-echo "\033[0;32m###########################################################\032[0m"
-echo "\033[0;32mPASO 12. HABILITAR LOS COMANDOS DE LNMS\032[0m"
-echo "\033[0;32m###########################################################\032[0m"
+echo "\033[0;32m###########################################################"
+echo "\033[0;32mPASO 12. HABILITAR LOS COMANDOS DE LNMS"
+echo "\033[0;32m###########################################################"
 # Enable lnms command completion
 sleep 3
 ln -s /opt/librenms/lnms /usr/bin/lnms
 cp /opt/librenms/misc/lnms-completion.bash /etc/bash_completion.d/
 echo " "
-echo "\033[0;32m###########################################################\032[0m"
-echo "\033[0;32mPASO 13. CONFIGURAR SNMPD\032[0m"
-echo "\033[0;32m###########################################################\032[0m"
+echo "\033[0;32m###########################################################"
+echo "\033[0;32mPASO 13. CONFIGURAR SNMPD"
+echo "\033[0;32m###########################################################"
 # Configure snmpd
 sleep 3
 cp /opt/librenms/snmpd.conf.example /etc/snmp/snmpd.conf
@@ -164,16 +164,16 @@ chmod +x /usr/bin/distro
 systemctl enable snmpd
 systemctl restart snmpd
 echo " "
-echo "\033[0;32m###########################################################\032[0m"
-echo "\033[0;32mPASO 14. CONFIGURAR CRON\032[0m"
-echo "\033[0;32m###########################################################\032[0m"
+echo "\033[0;32m###########################################################"
+echo "\033[0;32mPASO 14. CONFIGURAR CRON"
+echo "\033[0;32m###########################################################"
 # Cron job
 sleep 3
 cp /opt/librenms/dist/librenms.cron /etc/cron.d/librenms
 echo " "
-echo "\033[0;32m###########################################################\032[0m"
-echo "\033[0;32mPASO 15. CONFIGURAR CALENDARIO\032[0m"
-echo "\033[0;32m###########################################################\032[0m"
+echo "\033[0;32m###########################################################"
+echo "\033[0;32mPASO 15. CONFIGURAR CALENDARIO"
+echo "\033[0;32m###########################################################"
 # Enable the scheduler
 sleep 3
 cp /opt/librenms/dist/librenms-scheduler.service /opt/librenms/dist/librenms-scheduler.timer /etc/systemd/system/
@@ -181,9 +181,9 @@ cp /opt/librenms/dist/librenms-scheduler.service /opt/librenms/dist/librenms-sch
 systemctl enable librenms-scheduler.timer
 systemctl start librenms-scheduler.timer
 echo " "
-echo "\033[0;32m###########################################################\032[0m"
-echo "\033[0;32mPASO 16. CONFIGURAR LOGROTAT\032[0m"
-echo "\033[0;32m###########################################################\032[0m"
+echo "\033[0;32m###########################################################"
+echo "\033[0;32mPASO 16. CONFIGURAR LOGROTAT"
+echo "\033[0;32m###########################################################"
 # Copy logrotate config
 sleep 3
 cp /opt/librenms/misc/librenms.logrotate /etc/logrotate.d/librenms
